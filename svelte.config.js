cbf98fe1e8cad2b3009ba3b05290b59915810dde
Het Shah <htshah60@gmail.com>
2021-12-20 18:30:05 -0500

    refactor: converted CJS imports to ES6 format

--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,17 @@
  * correctly highlight the CSS syntax and
  * avoid producing unwanted syntax errors.
  */
-const sveltePreprocess = require('svelte-preprocess');
+import sveltePreprocess from 'svelte-preprocess';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-module.exports = {
+export default {
     preprocess: sveltePreprocess({
         defaults:{
             style: 'postcss'
         },
         sourceMap: dev,
         postcss: true
-    }),
+    })
 };
