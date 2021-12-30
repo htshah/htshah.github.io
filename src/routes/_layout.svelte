eb411874d5459ce01ebe7a5acfa49e6e5a587ec6
Het Shah <htshah60@gmail.com>
2021-12-30 18:57:03 -0500

    feat: add 700 font wegiht for Poppins

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -8,7 +8,7 @@
 <svelte:head>
     <!-- Google Fonts -->
     <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;800&family=Fugaz+One&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&family=Fugaz+One&display=swap"
         rel="stylesheet"
     />
 </svelte:head>
